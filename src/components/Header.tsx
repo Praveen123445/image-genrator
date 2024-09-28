0915c1235d5ee9ece7a672365bcf622696e3a658
Praveen123445 <Pravin324@gmail.com>
2024-09-28 14:23:50 +0530

    changes in .env

---
+++ src/components/Header.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { Button } from "./ui/button";
+import { signIn, useSession } from "next-auth/react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { BiLoaderCircle } from "react-icons/bi";
+
+export default function Header() {
+  const [initialLoading, setInitialLoading] = useState<boolean>(true);
+  const { data: session, status } = useSession();
+
+  useEffect(() => {
+    if (status !== "loading") {
+      setInitialLoading(false);
+    }
+  }, [status, session]);
+  return (
+    <div className="fixed top-0 w-full h-[60px] bg-black border-b-2 border-white/60 p-3 flex justify-between items-center">
+      <Link href="/">
+        <h1 className="font-bold text-xl">ImageGen</h1>
+      </Link>
+      {initialLoading && status === "loading" ? (
+        <BiLoaderCircle className="animate-spin" />
+      ) : !session ? (
+        <div className="__menu">
+          <Button onClick={() => signIn("google")}>Login</Button>
+        </div>
+      ) : (
+        <Avatar>
+          <AvatarImage src={session.user?.image || ""} />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      )}
+    </div>
+  );
+}
+
+
